3f8ff059bb2b4cb03af3bb2f0117afe9f170ec03
GKhim <giakhiemvu19102002@gmail.com>
2023-04-19 09:53:47 +0700

    git commit -m "version 0.0.2"

--- BattleShip/inittable.js
+++ BattleShip/inittable.js
@@ -23,3 +23,29 @@ for (let i = 0; i < size; i++) {
 
 // Thêm bảng vào phần tử div có class "content"
 contentDiv.appendChild(table);
+
+// Lấy phần tử div có class "content" để thêm bảng vào
+contentDiv = document.querySelector('.RightContent');
+size = 10;
+// Tạo một đối tượng table
+table = document.createElement('table');
+table.id = 'computer-board';
+table.className = 'board';
+
+// Tạo các hàng trong bảng
+for (let i = 0; i < size; i++) {
+    let row = document.createElement('tr');
+
+    // Tạo các ô trong mỗi hàng
+    for (let j = 0; j < size; j++) {
+        let cell = document.createElement('td');
+        cell.className = 'board-cell';
+        row.appendChild(cell);
+    }
+
+    // Thêm hàng vào bảng
+    table.appendChild(row);
+}
+
+// Thêm bảng vào phần tử div có class "content"
+contentDiv.appendChild(table);
