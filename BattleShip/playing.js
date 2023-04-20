167a646d7dd898cf735853127e3062f5066f2ff8
GKhim <giakhiemvu19102002@gmail.com>
2023-04-20 17:43:59 +0700

    git commit -m "version 0.0.4 Running"

--- BattleShip/playing.js
+++ BattleShip/playing.js
@@ -1,5 +1,5 @@
 /*
-    New Section:
+    New Section: 1st
     Tạo bảng Computer
 */
 let rightContentDiv = document.querySelector('.RightContent');
@@ -33,7 +33,7 @@ for (let i = 0; i < size; i++) {
 rightContentDiv.appendChild(computerTable);
 
 /*
-    New Section:
+    New Section: Running 2nd
     Tạo bảng Player 
 */
 let leftContentDiv = document.querySelector('.LeftContent');
@@ -64,24 +64,114 @@ for (let i = 0; i < size; i++) {
 leftContentDiv.appendChild(playerTable);
 
 /*
+    New Section: Function
+    Check Valid Position
+*/
+
+checkPositionValid = function(x, y, table, shipSize, direction){
+    if(direction == "vertical"){
+        table.forEach(function(computerCell){
+            for(let i = 0; i < shipSize; i++){
+                if(computerCell.dataset.col === (x+i) && computerCell.dataset.row === y){
+                    return false;
+                }
+            }
+        })
+    }
+
+    if(direction == "horizon"){
+        table.forEach(function(computerCell){
+            for(let i = 0; i < shipSize; i++){
+                if(computerCell.dataset.col ===x && computerCell.dataset.row === (y+i)){
+                    return false;
+                }
+            }
+        })
+    }
+
+    return true;
+}
+
+/*
+    New Section:
+    randomDirection
+*/
+
+randomDirection = function(){return Math.floor(Math.random() * 2) == 0 ? "horizon" : "vertical";};
+
+/*
+    New Section:
+    Random Computer position Ship
+*/
+let computer_Board = document.querySelectorAll("#computerCell");
+let computer_Ship = [3,4];
+let computer_Ship_Position = [];
+
+computer_Ship.forEach(function(shipSize){
+    let x = Math.floor(Math.random()*size);
+    let y = Math.floor(Math.random()*size);
+    let direction = randomDirection();
+    console.log(x + " " + y + " " + direction + shipSize);
+    console.log(checkPositionValid(x,y,computer_Board,shipSize,direction));
+    while(checkPositionValid(x,y,computer_Board,shipSize,direction)==false){
+        x = Math.floor(Math.random()*size);
+        y = Math.floor(Math.random()*size);
+        direction = randomDirection();
+    }
+
+    let positions = [];
+    console.log(x + " " + y + " " + direction);
+
+    if(direction == "vertical"){
+        computer_Board.forEach(function(computerCell){
+            for(let i = 0; i < shipSize; i++){
+                console.log("vertical run");
+                if(computerCell.dataset.col == (x+i) && computerCell.dataset.row == y){
+                    computerCell.dataset.value = 1;
+                    positions.push(computerCell);
+                    console.log(computerCell.dataset.col + " " + computerCell.dataset.row);
+                }
+            }
+        })
+    } else {
+        computer_Board.forEach(function(computerCell){
+        for(let i = 0; i < shipSize; i++){
+            console.log("horizon run");
+            if(computerCell.dataset.col === (x) && computerCell.dataset.row === (y+i)){
+                computerCell.dataset.value = 1;
+                positions.push(computerCell);
+                console.log(computerCell.dataset.col + " " + computerCell.dataset.row);
+            }
+        }
+        })
+    }
+    computer_Ship_Position.push(positions);
+
+});
+
+/*
     New Section:
     Point Cell Action
 */
-// Game bắt đầu từ đây
+
+// Game starts here
 
 //Player attack on ComputerZone
-let computer_Board = document.querySelectorAll("#computerCell")
-
 computer_Board.forEach(function(computerCell) {
     computerCell.addEventListener("click", function() {
+        console.log(computerCell.dataset.value);
         if(computerCell.dataset.value==0){
             computerCell.classList.add("selected");
-            computerCell.dataset.value = 1;
-            console.log(computer_Board);
+            computerCell.id = "Empty";
+            computerCell.dataset.value = -1;
+        } else if(computerCell.dataset.value==1){
+            computerCell.classList.add("selected");
+            computerCell.id = "Ship";
+            computerCell.dataset.value = -1;
         }
+        console.log(computerCell.dataset.value);
     });
 });
-
 /*
     New Section:
     Turn Action
